f814d049aa140c7c2e9df2ee9e0e202b325c6be8
Tewprai Bualoi <tewprai.b@7solutions.co.th>
2025-07-28 15:09:44 +0700

    fix: update title for better branding and add favicon

--- src/assets/texts.ts
+++ src/assets/texts.ts
@@ -82,83 +82,651 @@ export const texts = [
   },
   {
     text: "hello",
-    time: "12:19",
+    time: "16:13",
   },
   {
     text: "how are you",
-    time: "12:20",
+    time: "16:14",
   },
   {
     text: "hey",
-    time: "12:21",
+    time: "16:14",
   },
   {
     text: "where are you now",
-    time: "12:21",
+    time: "16:14",
   },
   {
     text: "will you marry me",
-    time: "12:22",
+    time: "16:14",
   },
   {
     text: "ok",
-    time: "12:23",
+    time: "16:14",
   },
   {
     text: "good bye",
-    time: "12:24",
+    time: "16:14",
   },
   {
     text: "miss u please answer :0",
-    time: "12:25",
+    time: "16:16",
   },
   {
     text: "hello",
-    time: "12:26",
+    time: "16:17",
   },
   {
     text: "no",
-    time: "12:27",
+    time: "16:18",
   },
   {
     text: "how are  today",
-    time: "12:28",
+    time: "16:18",
   },
   {
     text: "sawetdee kub",
-    time: "12:29",
+    time: "16:22",
   },
   {
     text: "tum arai u",
-    time: "12:30",
+    time: "16:22",
   },
   {
     text: "tob noi kub",
-    time: "12:31",
+    time: "16:22",
   },
-  { text: "T_T", time: "12:32" },
+  { text: "T_T", time: "16:23" },
   {
     text: "mai tong bin hai soung ysng ksi kao",
-    time: "12:33",
+    time: "16:24",
   },
   {
     text: "mama, i'm on a bill",
-    time: "12:34",
+    time: "16:25",
   },
   {
     text: "sybau...im crying...",
-    time: "12:35",
+    time: "16:26",
   },
   {
     text: "type shit",
-    time: "12:36",
+    time: "16:26",
   },
   {
     text: "shit",
-    time: "12:37",
+    time: "16:26",
   },
   {
     text: "ouo hola",
-    time: "12:38",
+    time: "16:27",
+  },
+  {
+    text: "i sus gu la kod ruk mueng loei",
+    time: "16:28",
+  },
+  {
+    text: "haha",
+    time: "16:28",
+  },
+  {
+    text: "just kidding",
+    time: "16:28",
+  },
+  {
+    text: "please find me via 888",
+    time: "16:28",
+  },
+  {
+    text: "i give you everything",
+    time: "16:29",
+  },
+  {
+    text: "money! ni i give a fvck",
+    time: "16:29",
+  },
+  {
+    text: "i love yo babe marry me pleaseeeeeeeee",
+    time: "16:30",
+  },
+  {
+    text: "marry meeeeeee",
+    time: "16:30",
+  },
+  {
+    text: "pleaseeeeee",
+    time: "16:30",
+  },
+  {
+    text: "why you ignore me",
+    time: "16:31",
+  },
+  {
+    text: "hello world.",
+    time: "16:36",
+  },
+  {
+    text: "hhhhh",
+    time: "16:49",
+  },
+  {
+    text: "iiiiiiiiii",
+    time: "16:49",
+  },
+  {
+    text: "iiiiiiiii",
+    time: "16:49",
+  },
+  {
+    text: "dddddddddd",
+    time: "16:49",
+  },
+  {
+    text: "uuuuuudjfhggh",
+    time: "16:49",
+  },
+  {
+    text: "helllllllooooo",
+    time: "16:49",
+  },
+  {
+    text: "luv",
+    time: "16:52",
+  },
+  {
+    text: "m",
+    time: "16:53",
+  },
+  {
+    text: "goethe",
+    time: "16:53",
+  },
+  {
+    text: "ich liebe dich",
+    time: "16:54",
+  },
+  {
+    text: "ich vermisse dich",
+    time: "16:55",
+  },
+  {
+    text: "moooooo",
+    time: "16:55",
+  },
+  {
+    text: "ich liebe dich auch",
+    time: "16:56",
+  },
+  {
+    text: "at goetfv",
+    time: "17:04",
+  },
+  {
+    text: "wyd",
+    time: "17:04",
+  },
+  {
+    text: "hallo",
+    time: "17:07",
+  },
+  {
+    text: "oooooo",
+    time: "17:07",
+  },
+  {
+    text: "hola como est",
+    time: "17:07",
+  },
+  {
+    text: "hiiiiiiiii",
+    time: "17:11",
+  },
+  {
+    text: "hello",
+    time: "17:11",
+  },
+  {
+    text: "hi",
+    time: "17:12",
+  },
+  {
+    text: "make me sweat",
+    time: "17:12",
+  },
+  {
+    text: "make me water",
+    time: "17:12",
+  },
+  {
+    text: "make me lose my breath",
+    time: "17:12",
+  },
+  {
+    text: "make me waterrrrrrrr",
+    time: "17:12",
+  },
+  {
+    text: "hey",
+    time: "17:17",
+  },
+  {
+    text: "how u been",
+    time: "17:17",
+  },
+  {
+    text: "how's brussels",
+    time: "17:18",
+  },
+  {
+    text: "yiu",
+    time: "17:18",
+  },
+  {
+    text: "can I ask u out sometime",
+    time: "17:19",
+  },
+  {
+    text: "are we together",
+    time: "17:19",
+  },
+  {
+    text: "hi scorpio' his is taurus",
+    time: "17:22",
+  },
+  {
+    text: "ive been cornered to flirt with you",
+    time: "17:22",
+  },
+  {
+    text: "gemini told me he doesnt want to see yo agaim",
+    time: "17:23",
+  },
+  {
+    text: "gemini does not  want to wate your time",
+    time: "17:24",
+  },
+  {
+    text: "very un scorpio of you to ghost me",
+    time: "17:25",
+  },
+  {
+    text: "heyyyyyyyy",
+    time: "17:30",
+  },
+  {
+    text: "youuuuuuu",
+    time: "17:30",
+  },
+  {
+    text: "chillinggggg",
+    time: "17:30",
+  },
+  {
+    text: "hi i am ajarn nat from kmitl",
+    time: "17:31",
+  },
+  {
+    text: "yoooo",
+    time: "17:31",
+  },
+  {
+    text: "hi[;",
+    time: "17:33",
+  },
+  {
+    text: "hihihi",
+    time: "17:33",
+  },
+  {
+    text: ";o",
+    time: "17:34",
+  },
+  {
+    text: "r u gemini",
+    time: "17:34",
+  },
+  {
+    text: ".....",
+    time: "17:34",
+  },
+  {
+    text: ".",
+    time: "17:34",
+  },
+  {
+    text: ".",
+    time: "17:34",
+  },
+  {
+    text: ".",
+    time: "17:34",
+  },
+  {
+    text: ".",
+    time: "17:34",
+  },
+  {
+    text: ".",
+    time: "17:34",
+  },
+  {
+    text: ".",
+    time: "17:34",
+  },
+  {
+    text: ".",
+    time: "17:34",
+  },
+  {
+    text: ".",
+    time: "17:34",
+  },
+  {
+    text: ".",
+    time: "17:34",
+  },
+  {
+    text: "..",
+    time: "17:34",
+  },
+  {
+    text: ".",
+    time: "17:34",
+  },
+  {
+    text: ".",
+    time: "17:34",
+  },
+  {
+    text: ".",
+    time: "17:34",
+  },
+  {
+    text: ".m",
+    time: "17:34",
+  },
+  {
+    text: ".",
+    time: "17:34",
+  },
+  {
+    text: ".",
+    time: "17:34",
+  },
+  {
+    text: ".",
+    time: "17:34",
+  },
+  {
+    text: "..",
+    time: "17:34",
+  },
+  {
+    text: "..",
+    time: "17:34",
+  },
+  {
+    text: "m;",
+    time: "17:34",
+  },
+  {
+    text: "yoyoyoyoo",
+    time: "17:35",
+  },
+  {
+    text: "bye",
+    time: "17:35",
+  },
+  {
+    text: ";n",
+    time: "17:35",
+  },
+  {
+    text: "hello",
+    time: "17:36",
+  },
+  {
+    text: "hi",
+    time: "17:36",
+  },
+  {
+    text: "i",
+    time: "17:38",
+  },
+  {
+    text: "?????",
+    time: "17:38",
+  },
+  {
+    text: "??????",
+    time: "17:39",
+  },
+  {
+    text: "???",
+    time: "17:39",
+  },
+  {
+    text: "i'",
+    time: "17:39",
+  },
+  {
+    text: "ii",
+    time: "17:39",
+  },
+  {
+    text: "i",
+    time: "17:39",
+  },
+  {
+    text: "i",
+    time: "17:39",
+  },
+  {
+    text: "i",
+    time: "17:39",
+  },
+  {
+    text: "i",
+    time: "17:39",
+  },
+  {
+    text: "i",
+    time: "17:39",
+  },
+  {
+    text: "i",
+    time: "17:39",
+  },
+  {
+    text: "u",
+    time: "17:39",
+  },
+  {
+    text: "i",
+    time: "17:39",
+  },
+  {
+    text: "ii",
+    time: "17:39",
+  },
+  {
+    text: "i",
+    time: "17:39",
+  },
+  {
+    text: "i",
+    time: "17:39",
+  },
+  {
+    text: "ii",
+    time: "17:39",
+  },
+  {
+    text: "i",
+    time: "17:39",
+  },
+  {
+    text: "i",
+    time: "17:39",
+  },
+  {
+    text: "i",
+    time: "17:39",
+  },
+  {
+    text: "ij'j''",
+    time: "17:39",
+  },
+  {
+    text: "h hw are. you",
+    time: "17:41",
+  },
+  {
+    text: "nice seeimg u",
+    time: "17:41",
+  },
+  {
+    text: "hi krub",
+    time: "17:46",
+  },
+  {
+    text: "how are you girl",
+    time: "17:47",
+  },
+  {
+    text: "text me back when ready you are",
+    time: "17:47",
+  },
+  {
+    text: "...",
+    time: "17:47",
+  },
+  {
+    text: "yo",
+    time: "17:48",
+  },
+  {
+    text: "hi",
+    time: "17:49",
+  },
+  {
+    text: "how r u",
+    time: "17:49",
+  },
+  {
+    text: "u still up",
+    time: "17:50",
+  },
+  {
+    text: "hey",
+    time: "17:51",
+  },
+  {
+    text: "hows ur day",
+    time: "17:51",
+  },
+  {
+    text: "hope u doing well",
+    time: "17:51",
+  },
+  {
+    text: "i love u",
+    time: "17:52",
+  },
+  {
+    text: "i miss u",
+    time: "17:52",
+  },
+  {
+    text: "i miss u",
+    time: "17:53",
+  },
+  {
+    text: "wanna meet u",
+    time: "17:53",
+  },
+  {
+    text: "heyyy",
+    time: "17:53",
+  },
+  {
+    text: "i wanna see you",
+    time: "17:53",
+  },
+  {
+    text: "babyy",
+    time: "17:54",
+  },
+  {
+    text: "sawadee kub",
+    time: "17:54",
+  },
+  {
+    text: "sabai d bor",
+    time: "17:54",
+  },
+  {
+    text: "hi bo",
+    time: "17:56",
+  },
+  {
+    text: "howdy?",
+    time: "17:57",
+  },
+  {
+    text: "dinner yeti",
+    time: "17:57",
+  },
+  {
+    text: "*yet?",
+    time: "17:58",
+  },
+  {
+    text: "wanna ga",
+    time: "17:58",
+  },
+  {
+    text: "wanna r",
+    time: "17:59",
+  },
+  {
+    text: "i don't ccare you animore bro",
+    time: "18:00",
+  },
+  {
+    text: "ming ming",
+    time: "18:02",
+  },
+  {
+    text: "what up",
+    time: "18:03",
+  },
+  {
+    text: "hahahaha",
+    time: "18:03",
+  },
+  {
+    text: "xxxxxxx",
+    time: "18:03",
+  },
+  {
+    text: "hahahahaha",
+    time: "18:03",
+  },
+  {
+    text: "i love you",
+    time: "18:04",
+  },
+  {
+    text: "eieiza55+",
+    time: "18:04",
+  },
+  {
+    text: "let's not fuck anymore",
+    time: "18:16",
+  },
+  {
+    text: "more juicy stuff is adding, stay tuned",
+    time: "<3",
   },
 ];
